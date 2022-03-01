cea6a8b8e0ec0e31a6d56b1a908da614cd0ee565
deltoro7 <98106577+deltoro7@users.noreply.github.com>
2022-02-28 23:05:14 -0500

    First commit
    (Test output could not get the Matrix into Echelon Form

---
+++ gaus1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+A1 = [1, 2, 13]
+A2 = [3, 4, 29]
+A = [A1, A2]
+print(A)
+n = len(A)
+
+
+
+#def solve(A, n):
+row_used = 0
+row_work_on= 0
+for row_used in range(0,n):
+
+    print("First Test")
+
+    for row_work_on in range(row_used +1, n):
+
+        print("Second Test")
+
+        elimination_column = row_work_on -1
+
+        print(A[row_work_on][elimination_column])
+
+        elimination_factor = -1 * A[row_work_on][elimination_column] / A[row_used][row_used]
+        print(elimination_factor)
+
+        row_worked_on_column = elimination_column
+        print(row_worked_on_column)
+
+        for row_worked_on_column in range(row_work_on-1, n+1):
+            print("Third test")
+            A[row_work_on][row_worked_on_column] = elimination_factor * A[row_used][row_worked_on_column] + A[row_work_on][row_worked_on_column]
+        A[row_used][row_work_on] = A[row_worked_on_column][elimination_column] + A[row_used][row_work_on]
+
+
+
+
+                #return(A)
+print(A)
